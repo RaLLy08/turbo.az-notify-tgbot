021ef12824c2f5ca17bba741bc6256d731858063
azadk@forit.az <uytemataaero@mail.ru>
2021-03-22 22:15:14 +0400

    animate to ts

--- src/telegram/AnimateText.ts
+++ src/telegram/AnimateText.ts
@@ -1,9 +1,20 @@
-class AnimateText { 
+interface AnimateParamsInterface {
+    frameMs: number,
+    animation: Array<string>,
+    animationCallback: (animateState: string) => void
+}
+
+class AnimateText {
+    readonly frameMs: number;
+    readonly animation: Array<string>;
+    readonly animationCallback: (animateState: string) => void;
+    private timeout: ReturnType<typeof setTimeout>;
+
     constructor({
         frameMs, // <Number>
         animation, // <Array>
         animationCallback // <Function>
-    }) {
+    }: AnimateParamsInterface) {
         this.frameMs = frameMs;
         this.animation = animation;
         this.animationCallback = animationCallback;
@@ -14,10 +25,10 @@ class AnimateText {
     }
 
     startLinearAnimate() {
-        let end = 0;
+        let end: number = 0;
 
         this.timeout = setInterval(() => {
-            const animateState = this.animation.join('').substring(0, end);
+            const animateState: string = this.animation.join('').substring(0, end);
 
             this.animationCallback(animateState);
 
@@ -30,22 +41,22 @@ class AnimateText {
     }
 
     startRandomAnimate() {
-        let end = 0;
+        let end: number = 0;
 
         this.timeout = setInterval(() => {
-            const animateState = this.animation.join('').substring(0, end);
+            const animateState: string = this.animation.join('').substring(0, end);
 
             this.animationCallback(animateState);
 
-            end =  Math.random() * (this.animation.length - 1) + 1;
+            end = Math.random() * (this.animation.length - 1) + 1;
         }, this.frameMs);
     }
 
     startFrameAnimate() {
-        let end = 0;
+        let end: number = 0;
 
         this.timeout = setInterval(() => {
-            const animateState = this.animation.join('')[end];
+            const animateState: string = this.animation.join('')[end];
 
             this.animationCallback(animateState);
 
